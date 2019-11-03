6ccf208b95050d1a2d495dc42e66578123e74afb
mac <mac@MacBook-Air-de-mac-2.local>
2019-11-02 23:55:20 -0400

    add deuteron

--- ParticleCollision/src/particlecollision/AssetManager.java
+++ ParticleCollision/src/particlecollision/AssetManager.java
@@ -1,5 +1,3 @@
-package particlecollision;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
@@ -14,21 +12,27 @@ import javafx.scene.media.Media;
 import javafx.scene.paint.ImagePattern;
 
 public class AssetManager {
-
-    static protected Background backgroundImage;
-    static protected ImagePattern logo;
-    static protected ImagePattern proton;
-    static protected Media backgroundMusic;
-
-    static private String fileURL(String relativePath) {
-        return new File(relativePath).toURI().toString();
-    }
-
-    static public void preloadAllAssets() {
-        Image background = new Image(fileURL("./assets/backgroundsmall.png"));
-        backgroundImage = new Background(new BackgroundImage(background,BackgroundRepeat.ROUND,BackgroundRepeat.ROUND,BackgroundPosition.DEFAULT,BackgroundSize.DEFAULT));
-        logo = new ImagePattern(new Image(fileURL("./assets/logo.png")));
-        proton = new ImagePattern(new Image(fileURL("./assets/proton.png")));
-    }
-
+	 static private Background backgroundImage = null;
+	 static private ImagePattern logo = null;
+	 static protected ImagePattern proton = null;
+	 static private Media backgroundMusic = null;
+	 static private ImagePattern deuteron =null;
+	  static private String fileURL(String relativePath)
+	    {
+	        return new File(relativePath).toURI().toString();
+	    }
+	 static public void preloadAllAssets()
+	    {
+		 Image background = new Image(fileURL("./assets/background.png"));
+		 backgroundImage = new Background(
+                 new BackgroundImage(background, 
+                                     BackgroundRepeat.ROUND, 
+                                     BackgroundRepeat.ROUND, 
+                                     BackgroundPosition.DEFAULT,
+                                     BackgroundSize.DEFAULT));
+		 logo = new ImagePattern(new Image(fileURL("./assets/logo.png")));
+		 proton = new ImagePattern(new Image(fileURL("./assets/proton.png")));
+		 deuteron = new ImagePattern(new Image(fileURL("./assets/deuteron.png")));
+	    }
+	 
 }
